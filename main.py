03710ab3c7fa57d395738573b45970fae2c6a360
simek456 <w.simek0147@gmail.com>
2021-06-06 10:35:37 +0200

    test

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import fhirpy as fhir
 from fhirpy import SyncFHIRClient
 
+HAPI_BASE_URL = "http://hapi.fhir.org/baseR4"
+
 if __name__ == "__main__":
+    client = SyncFHIRClient(HAPI_BASE_URL)
+    resources = client.resources('Patient')
+    resources = resources.limit(10).sort('name')
+    patients = resources.fetch()
+    me = patients.pop()
+    print(me['name'][0])
