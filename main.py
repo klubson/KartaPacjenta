54233a49bf6d9b5d27dca0db2875f3a3395d0274
simek456 <w.simek0147@gmail.com>
2021-06-14 14:34:01 +0200

    page movement - beginings

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import fhirpy as fhir
 from fhirpy import SyncFHIRClient
 from streamlit.components.v1 import html as st_html
 import streamlit as st
+import multipage_framework.multipage as mult
 
 HAPI_BASE_URL = "http://hapi.fhir.org/baseR4"
 
 
-def main():
+def main_page(prev_vars):
+    st.title("main page")
     client = SyncFHIRClient(
         HAPI_BASE_URL
     )
@@ -16,13 +18,29 @@ def main():
         patients = patients.search(family__contains=user_input).sort('name')
         results = patients.fetch()
         st.write("Results: " + str(len(results)))
+        st.markdown("---")
         for patient in results:
             dic = patient.serialize()
             name_s = ""
             for name in dic['name'][0]['given']:
                 name_s += name + " "
             st.write(name_s + dic['name'][0]['family'])
+            if st.button("Go", key=dic['id']):
+                mult.change_page(1)
+            st.markdown("---")
+
+
+def patient_page(prev_vars):
+    st.title("patient")
+    if st.button("go to main page"):
+        print("going main")
+        mult.change_page(0)
 
 
 if __name__ == "__main__":
-    main()
+    mult.start_app()
+    app = mult.MultiPage()
+    app.disable_navbar()
+    app.add_app("main page", main_page)
+    app.add_app("patient", patient_page)
+    app.run()
